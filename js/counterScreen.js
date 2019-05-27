e2ab19f50923367de40f2cfcc8db718c707eea15
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-05-27 14:35:52 +0200

    -

--- js/counterScreen.js
+++ js/counterScreen.js
@@ -1,27 +1,73 @@
 var countersAttack = document.getElementById("countersAttack");
 var countersDefense = document.getElementById("countersDefense");
 var hardCounters = document.getElementsByClassName("hardCounters");
+var row = document.getElementsByClassName("row");
 var hardCountersF;
-function setHardCounter(){
-  console.log(hardCountersF);
-  for(i=0;i<hardCountersF.length;i++){
-    hardCounters.innerHTML = hardCounters.innerHTML +
-    "<img class='operatorBadgeB' src='https://4d617474.github.io/siegeTool/badges/"
-    + hardCountersF[i] + ".svg'>";
+function setCounter(letter){
+  var countersSum = "";
+  var currentCounters;
+  if(letter=="B"){currentCounters = hardCountersF
+  }else{currentCounters = softCountersF}
+  console.log(countersSum);
+  //console.log(countersSum);
+  for(i=0;i<currentCounters.length;i++){
+    console.log(countersSum);
+    countersSum += "<img class='operatorBadge" + letter + "' src='badges/"
+    + currentCounters[i] + ".svg'>";
   }
+  //console.log(countersSum);
+  return countersSum;
 }
-function setCounter(op)
+// function sethCounter(){
+//   var countersSum;
+//   for(i=0;i<hardCountersF.length;i++){
+//     console.log(hardCountersF[i]);
+//     countersSum += "<img class='operatorBadgeB' src='badges/" +
+//     hardCountersF[i] + ".svg'>";
+//   }
+//   return countersSum;
+// }
+// function setsCounter(){
+//   var countersSum;
+//   for(i=0;i<softCountersF.length;i++){
+//     countersSum += "<img class='operatorBadgeC' src='badges/" +
+//     softCountersF[i] + ".svg'>";
+//   }
+//  return countersSum;
+// }
+function toggleCounter(op,state,side)
 {
-
+  //console.log(op,state,side);
+  if (op == "kapkan"){
+    hardCountersF = ["iq","thatcher","twitch"];
+    softCountersF = ["blitz","buck","finka","fuze","montagne","sledge","zofia"];
+  }
+  var sidePicked;
+  if (side == 1){
+    sidePicked = countersAttack;
+  }else{
+    sidePicked = countersDefense;
+  }
+  if(state == 1){
+    sidePicked.innerHTML = sidePicked.innerHTML +
+    '<div id="' + op + '" class="row">' +
+      '<div class="countered">' +
+        '<img class="operatorBadgeA" src="badges/' + op  + '.svg">' +
+      '</div>' +
+      '<div class="hardCounters">' + setCounter("B") + '</div>' +
+      '<div class="softCounters">' + setCounter("C") + '</div>' +
+      //'<div class="hardCounters">' + sethCounter() + '</div>' +
+      //'<div class="softCounters">' + setsCounter() + '</div>' +
+    '</div>';
+  }else{
+    //console.log(document.getElementById(op));
+    document.getElementById(op).remove();
+  };
   //if(atkSide.style.display == "none"){
-  //  countersDefense.innerHTML = countersDefense.innerHTML + "<div class='row'></div>";
   //}
   //if (op == ""){
   //setHardCounter("");
   //setSoftCounter("");
-  if (op == "kapkan"){
-  hardCountersF = ["iq","thatcher","twitch"];
-  }
-  setHardCounter();
+  //setHardCounter();
   //setSoftCounter();
 }
--- js/selectionScreen.js
+++ js/selectionScreen.js
@@ -17,18 +17,27 @@ function resetA()
   for (i = 0; i < operators.length; i++){
     operatorBadge[i].style.backgroundColor = "transparent";
   }
+  //console.log(typeof(row[0]));
+  currentRows = row.length;
+  if(typeof(row[0]) != 'undefined'){
+    for(i=0;i!=currentRows;i++){
+    row[0].remove();
+  }}
 }
 function clickOperator(){
-  console.log(event.target.alt);
-  setCounter(event.target.alt);
+  //console.log(event.target.alt);
   var pane = event.target.closest('.operatorBadge');
+  //console.log(pane.style.backgroundColor);
   if (pane.style.backgroundColor == "transparent"){
     if (atkSide.style.display == "block") {
+      toggleCounter(event.target.alt,1,1);
       pane.style.backgroundColor="#eb7116";
     } else {
+      toggleCounter(event.target.alt,1,2);
       pane.style.backgroundColor="#1489e0";
     }
   } else {
+    toggleCounter(event.target.alt,0,1);
     pane.style.backgroundColor="transparent";}
 }
 function switchSide(isdefside){
@@ -37,8 +46,13 @@ atkSide.style.display = "none";
  if (isdefside == 0){
    atkSide.style.display = "block";
    defSide.style.display = "none";
+   countersAttack.style.display = "block";
+   countersDefense.style.display = "none";
+
  } else {
    defSide.style.display = "block";
    atkSide.style.display = "none";
+   countersDefense.style.display = "block";
+   countersAttack.style.display = "none";
  }
 }
