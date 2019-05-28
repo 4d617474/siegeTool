18b3eff241b5a0967b338cb0038eb8d6579bb58b
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-05-28 11:59:11 +0200

    -

--- js/randomBackground.js
+++ js/randomBackground.js
@@ -1,4 +1,4 @@
 function randomBg() {
   var randNum = Math.floor(Math.random() * 10) + 1;
-  document.getElementById("background").style.backgroundImage= "url('backgrounds/map" + randNum + ".png')";
+  document.getElementById("background").style.backgroundImage= "url('backgrounds/map" + randNum + ".jpg')";
 }
