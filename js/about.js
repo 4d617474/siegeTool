cc530a23cb61f3663f44d8c466724964da20c0f6
4d617474 <33104312+4d617474@users.noreply.github.com>
2019-05-29 13:57:00 +0200

    -

--- js/about.js
+++ js/about.js
@@ -1,11 +1,14 @@
 function aboutFunction(){
   var abtFrame = document.getElementById("aboutFrame");
+  var abtCont = document.getElementById("aboutContainer");
   var sgTool = document.getElementById("siegeTool");
   if (abtFrame.style.display == "none"){
     abtFrame.style.display = "block";
+    abtCont.classList.add("aboutIntro");
     sgTool.style.filter = "blur(20px)";
   } else {
     abtFrame.style.display = "none";
+    abtCont.classList.remove("aboutIntro");
     sgTool.style.filter = "none";
   }
 }
